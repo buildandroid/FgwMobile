efa025f82fc2971d0cfc7b81809dc818b87f3490
jim <reachjim@gmail.com>
2016-06-11 14:32:02 +0800

    公文列表UI好了

--- app/src/main/java/activity/GwqpActivity.java
+++ app/src/main/java/activity/GwqpActivity.java
@@ -1,5 +1,6 @@
 package activity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,8 +10,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.fgwoa.fgwmobile.R;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -46,7 +50,7 @@ public class GwqpActivity extends AppCompatActivity {
     private class GwListAdapter extends BaseAdapter{
         @Override
         public int getCount() {
-            return 1;
+            return 20;
         }
 
         @Override
@@ -62,12 +66,36 @@ public class GwqpActivity extends AppCompatActivity {
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
             View view;
+            ViewHolder holder;
             if (convertView == null) {
                 view = LayoutInflater.from(getBaseContext()).inflate(R.layout.item_gwqp_list, parent, false);
+                holder = new ViewHolder();
+                holder.gongWenBiaoti = (TextView)view.findViewById(R.id.gongwen_biaoti);
+                holder.laiWenDanwei = (TextView)view.findViewById(R.id.laiwen_danwei);
+                holder.daiPi = (TextView)view.findViewById(R.id.daipi);
+                holder.laiWenRiqi = (TextView)view.findViewById(R.id.laiwen_riqi);
+                holder.leiXing = (TextView)view.findViewById(R.id.leixi);
+                view.setTag(holder);
             } else {
                 view = convertView;
+                holder = (ViewHolder)view.getTag();
             }
+            holder.gongWenBiaoti.setText("公文标题");
+            Resources res = getResources();
+            holder.laiWenDanwei.setText(String.format(res.getString(R.string.list_text_1), "北京 发 改"));
+            holder.daiPi.setText(String.format(res.getString(R.string.list_text_2), "张某某"));
+            holder.laiWenRiqi.setText(String.format(res.getString(R.string.list_text_3), "2016年12月22日"));
+            holder.leiXing.setText(String.format(res.getString(R.string.list_text_4), "发文"));
+
             return view;
+        }
+
+        class ViewHolder{
+            TextView gongWenBiaoti;
+            TextView laiWenDanwei;
+            TextView daiPi;
+            TextView laiWenRiqi;
+            TextView leiXing;
         }
     }
 
